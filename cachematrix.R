505b82dcb3f13d22eda2abc9271318ba0f95b3bf
Grant Oliveira <grant.oliveira>
2015-01-21 20:00:09 -0800

    Programming Assignment 2
    This assignment tested our understanding of lexical scoping, utilizing
    the <<- operator to define variables within a function that can later
    be used outside of it.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## These functions relate to matrix inversion. The first function inverts a given matrix
+## and caches the result:
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+set <- function(y){
+  x<<-y
+  m<<-NULL
+}
+get <- function() x
+setmatrix <- function(solve) m<<- solve
+getmatrix <- function() m
+list(set = set, get = get,
+     setmatrix = setmatrix,
+     getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## The second function checks to see if there is a matrix inversion already cached
+## and if so returns it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
+
